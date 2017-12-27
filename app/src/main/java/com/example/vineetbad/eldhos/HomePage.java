329b9606e834c7cd698283c1cf5107f3cd199605
vineetbad <vineetbad@gmail.com>
2017-12-27 14:36:19 -0800

    Before adding a dialog layout

--- app/src/main/java/com/example/vineetbad/eldhos/HomePage.java
+++ app/src/main/java/com/example/vineetbad/eldhos/HomePage.java
@@ -1,5 +1,7 @@
 package com.example.vineetbad.eldhos;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,7 +31,7 @@ public class HomePage extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String driving_options_selected = adapterView.getItemAtPosition(i).toString();
                 //Need to create an implement activity that can save something if not then cancel
-
+                AlertDialog.Builder driving_state_change_dialog = new AlertDialog.Builder(HomePage.this);
                 //Toast.makeText(adapterView.getContext(), driving_options_selected, Toast.LENGTH_SHORT).show();
             }
 
